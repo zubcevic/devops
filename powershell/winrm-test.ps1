fb2e9c45c8fab2170beda794a0bc78616c4e8dda
René Zubcevic <rene@zubcevic.com>
2020-09-27 15:50:32 +0200

    added script param

--- powershell/winrm-test.ps1
+++ powershell/winrm-test.ps1
@@ -1,3 +1,10 @@
+param(
+[parameter(Mandatory=$true)]
+[string] $MyServerName = ""
+)
+
+Write-Output ("Server: $($MyServerName)")
+
 $MySessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck
 
 $username = Read-Host "Enter username: "
@@ -5,7 +12,7 @@ $password = Read-Host -AsSecureString "Enter password: "
 
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$password
 
-Invoke-Command -ComputerName "demoserver" -ScriptBlock {
+Invoke-Command -ComputerName $MyServerName -ScriptBlock {
     [environment]::OSVersion
     $PSVersionTable
 } -Credential $credential -UseSSL -SessionOption $MySessionOption
